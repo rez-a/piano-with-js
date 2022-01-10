4e68618d086316d3837cce02558d1001f4cbd600
reza hasani <reza.hasani11177@gmail.com>
2022-01-10 18:49:42 +0330

    Add sound on piano

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 let keyboard = document.querySelector('.keys');
 let notePlayed = document.querySelector('.note-played')
 let keyList = document.querySelectorAll('.key');
+let hintList = keyboard.querySelectorAll('.hint');
 
 document.addEventListener('keydown', function(e) {
-    let key = keyboard.querySelector(`.key[data-code =${e.code}]`)
+    let key = keyboard.querySelector(`.key[data-code =${e.code}]`);
+    let audio = document.querySelector(`audio[data-code =${e.code}]`)
     if (!key) return;
 
     key.classList.add('played');
+    audio.currentTime = 0;
+    audio.play();
     let note = key.getAttribute('data-note');
     notePlayed.innerHTML = note;
 })
@@ -15,4 +19,7 @@ keyList.forEach(key => {
     key.addEventListener('transitionend', function() {
         this.classList.remove('played');
     })
+})
+hintList.forEach((hint, index) => {
+    hint.setAttribute('style', `transition-delay:${index*50}ms`);
 })

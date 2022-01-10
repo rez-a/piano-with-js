c49904d7f7417302c2c8fb3152b0c3234f341d2f
reza hasani <reza.hasani11177@gmail.com>
2022-01-10 17:14:47 +0330

    Transition event management

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 let keyboard = document.querySelector('.keys');
 let notePlayed = document.querySelector('.note-played')
+let keyList = document.querySelectorAll('.key');
 
 document.addEventListener('keydown', function(e) {
     let key = keyboard.querySelector(`.key[data-code =${e.code}]`)
-
     if (!key) return;
 
+    key.classList.add('played');
     let note = key.getAttribute('data-note');
     notePlayed.innerHTML = note;
+})
+
+keyList.forEach(key => {
+    key.addEventListener('transitionend', function() {
+        this.classList.remove('played');
+    })
 })
